70e7a5cf9caf95c917515e8673a038b09e01fcb1
Axel7755 <baxeltomas@gmail.com>
2023-11-26 14:11:00 -0600

    ver Sprints

---
+++ php/verSprint.php
@@ -0,0 +1,111 @@
+<?php
+require "../../php/conexion.php";
+
+$proyecto = $_GET['proy'];
+$sqlVerSrpints = "SELECT * FROM sprint WHERE spr_idproyect = '$proyecto'";
+$res = $con->query($sqlVerSrpints);
+if ($res->num_rows > 0) {
+    while ($row = $res->fetch_assoc()){
+        echo '<div class="row layout bg-grey rounded-3">
+        <div class="col element">
+            <div class="row">
+                <div class="col-lg-2">
+                    <button class="btn dropdown-toggle" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#SprintId" aria-expanded="false" aria-controls="collapseExample">
+                        Tablero '.$row["nombre_sp"].'
+                    </button>
+                </div>
+                <div class="col-lg-2">
+                    <button class="btn" type="button" data-bs-toggle="modal" data-bs-target="#EditarSprint1">
+                        <i class="bi bi-pencil"></i> Editar Sprint
+                    </button>
+                </div>
+                <div class="col-lg-2">
+                    <button class="btn add-sprint-button" type="button">
+                        Iniciar Sprint
+                    </button>
+                </div>
+                <div class="col text-end">
+                    <div class="dropdown">
+                        <button class="btn drop" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-three-dots"></i>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    Eliminar Sprint
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    Creacion o cambios recientes
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+
+                                    Creacion o cambios recientes
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row collapse" id="SprintId">
+                <div class="col">
+                    <div class="row punteado rounded-3 contenido">
+                        <div class="col margin0">
+                            <div class="row incidencia-cont">
+                                <div class="col-9">
+                                    <p class="incidencia-text">Nombre de incidencia</p>
+                                </div>
+                                <div class="col-lg-2 ">
+                                    <select class="form-select empty-background completo"
+                                        aria-label="Default select example" id="t-incidencia">
+                                        <option selected value="1">Por hacer</option>
+                                        <option value="2">En progreso</option>
+                                        <option value="3">Listo</option>
+                                    </select>
+                                </div>
+                                <div class="col">
+                                    <div class="dropdown">
+                                        <button class="btn drop" type="button" data-bs-toggle="dropdown"
+                                            aria-expanded="false">
+                                            <i class="bi bi-three-dots"></i>
+                                        </button>
+                                        <ul class="dropdown-menu margin0">
+                                            <li class="">
+                                                <a class="dropdown-item" href="#" data-bs-toggle="modal"
+                                                    data-bs-target="#incidenciaCrear">
+                                                    <label class="form-check-label" for="MisIncidencias-f">
+                                                        Editar
+                                                    </label>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a class="dropdown-item" href="#">
+                                                    <label class="form-check-label" for="MisIncidencias-f">
+                                                        Eliminar
+                                                    </label>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col btnincidencias">
+                            <button type="button" class="btn add-button" data-bs-toggle="modal"
+                                data-bs-target="#incidenciaCrear">
+                                <p class="element"><i class="bi bi-plus"></i> Crear incidencia</p>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>';
+    }
+}
--- php/visualizarProyectos.php
+++ php/visualizarProyectos.php
@@ -2,43 +2,43 @@
 require "../php/conexion.php";
 $tipo = $_SESSION['Tipo'];
 $id = $_SESSION['ID'];
-switch ($tipo){
-    case 0:
-        $sqlVproy = "SELECT * FROM `proyecto` proy JOIN `proyecto_alumno` proyal 
+switch ($tipo) {
+  case 0:
+    $sqlVproy = "SELECT * FROM `proyecto` proy JOIN `proyecto_alumno` proyal 
         ON (proy.idproyect=proyal.pa_idproyect) WHERE pa_idalumno = '$id'";
-        break;
-    case 1:
-        $sqlVproy = "SELECT * FROM `proyecto` WHERE solicitante_idsolicitante = '$id'";
-        break;
-    case 2:
-        $sqlVproy = "SELECT * FROM `proyecto`";
-        break;
+    break;
+  case 1:
+    $sqlVproy = "SELECT * FROM `proyecto` WHERE solicitante_idsolicitante = '$id'";
+    break;
+  case 2:
+    $sqlVproy = "SELECT * FROM `proyecto`";
+    break;
 }
 $sqlVproy = "SELECT * FROM `proyecto`";
 $res = $con->query($sqlVproy);
-    if ($res->num_rows > 0) {
-        while ($row = $res->fetch_assoc()) {
-            echo '<div class="col proy">
-            <a class="empty" href="./proyectos/backlog.php?proy='.$row["idproyect"].'">
+if ($res->num_rows > 0) {
+  while ($row = $res->fetch_assoc()) {
+    echo '<div class="col proy">
+            <a class="empty" href="./proyectos/backlog.php?proy=' . $row["idproyect"] . '">
               <div class="container text-center">
                 <div class="row">
                   <div class="col left">
-                    <img class="img-psrc" src="'.$row["img"].'">
+                    <img class="img-psrc" src="' . $row["img"] . '">
                   </div>
                   <div class="col-8 left">
-                    <p class="tittle-p">'.$row["nombrePr"].'</p>
+                    <p class="tittle-p">' . $row["nombrePr"] . '</p>
                   </div>
                 </div>
                 <div class="row">
                   <div class="col left">
                     <p class="subtittle-p">Descripcion:</p>
-                    <p class="text-p">'.$row["proyectcol"].'</p>
+                    <p class="text-p">' . $row["proyectcol"] . '</p>
                   </div>
                 </div>
               </div>
             </a>
           </div>';
-        }
-    }else{
-        echo "<br><p style='color: red;'>sin proyectos visibles</p>";
-    }
+  }
+} else {
+  echo "<br><p style='color: red;'>sin proyectos visibles</p>";
+}
