0423b55cd8f78dd3d3e2c84352bfb45872752f7e
AlanRicardoCalderonMtz <AlanRicardoCM@gmail.com>
2024-02-23 17:07:25 -0600

    Finalice el tablero con diseño e interacciones

--- pages/proyectos.php
+++ pages/proyectos.php
@@ -21,8 +21,7 @@ include "../php/crearProyecto.php";
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <!-- Google Font Link Icons-->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-   <!--My CSS-->
-   <link href="./css/style.css" rel="stylesheet">
+  <!--My CSS-->
   <link href="../css/style.css" rel="stylesheet">
   <link href="../css/proyectos.css" rel="stylesheet">
 </head>
