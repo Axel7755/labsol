8a06852a35d39091c739beef13db2ff51cd9e47c
Axel7755 <baxeltomas@gmail.com>
2024-04-08 13:56:25 -0600

    eliminacion de miembros y redireccionamientos al inicio

--- php/agregarAlumnoProy.php
+++ php/agregarAlumnoProy.php
@@ -2,22 +2,22 @@
 require "../../php/conexion.php";
 if (isset($_POST['agregarAlumnos'])) {
     if (!empty($_POST['Alumnos'])) {
-        foreach ($_POST['Alumnos'] as $Alumno) {
+        foreach ($_POST['Alumnos'] as $alumno) {
             $proyecto = $_GET['proy'];
             $sqlAgregarAlumosProy = "INSERT INTO `proyecto_alumno` (`pa_idproyect`,`pa_idalumno`)VALUES
-                    ('$proyecto','$Alumno')";
+                    ('$proyecto','$alumno')";
             //echo"$sql";
             try {
                 if ($con->query($sqlAgregarAlumosProy) == true) {
                     
                 } else {
-                    echo "<br><p style='color: blue;'>Error al asignar el grupo</p>";
+                    echo "<br><p style='color: blue;'>Error al asignar al grupo</p>";
                 }                
             } catch (mysqli_sql_exception $e) {
                 echo "<br><p style='color: red;'>Error,asignacion ya existente</p>";
             }
         }
     } else {
-        echo "<br><p style='color: blue;'>Error, selecciona al menos una Materia</p>";
+        echo "<br><p style='color: blue;'>Error, selecciona al menos un Alumno</p>";
     }
 }
