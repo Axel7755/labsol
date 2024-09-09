6910ec0a122c907344a8924c209701112053bff0
Axel7755 <baxeltomas@gmail.com>
2024-09-09 07:49:24 -0600

    cambios con errores

--- js/subincidencias.js
+++ js/subincidencias.js
@@ -1,11 +1,11 @@
 //const addBtn = document.querySelector(".add");
 
 
-function removeInput(){
+function removeInput() {
     this.parentElement.remove();
     i--;
 }
-var i=0;
+var i = 0;
 
 var botonesDelSub = document.querySelectorAll(".deleteSub");
 botonesDelSub.forEach(botonDelSub => {
@@ -15,59 +15,63 @@ botonesDelSub.forEach(botonDelSub => {
 /*function addInput(event){
     event.preventDefault();*/
 
-    //Con el final ed del modal de edicion identificar el numero de ning para el valor i 
-function addInput(idSprint){
-    const input = document.querySelector(".subincidencias-group"+idSprint);
+//Con el final ed del modal de edicion identificar el numero de ning para el valor i 
+function addInput(idSprint) {
+    const input = document.querySelector(".subincidencias-group" + idSprint);
+    //var edicion = idSprint.substring(idSprint.length - 1);
+        if ( esNumerico(input)) {
+            console.log("edlocalizaado!");
+            const elements = input.getElementsByName("ning");
+            if (elements.length === 0) {
+
+                console.log("no existe!");
+                
+            } else {
+                console.log(" existe!");
+                i = elements.value;
+            }
+        }
     //preventDefault();
     i++;
     const name = document.createElement("input");
-    name.type="text";
-    name.className="form-control";
+    name.type = "text";
+    name.className = "form-control";
     name.placeholder = "Nombre de subincidencia";
-    name.name="nombreSub"+i;
+    name.name = "nombreSub" + i;
 
     const labelDesc = document.createElement("label");
-    labelDesc.textContent="Descripcion";
+    labelDesc.textContent = "Descripcion";
 
     const descripcion = document.createElement("textarea");
-    descripcion.cols="45";
-    descripcion.rows="10";
-    descripcion.className="form-control";
+    descripcion.cols = "45";
+    descripcion.rows = "10";
+    descripcion.className = "form-control";
     //descripcion.placeholder = "Digite su cantidad";
-    descripcion.name="descrip"+i;
+    descripcion.name = "descrip" + i;
 
-    const btn=document.createElement("a");
+    const btn = document.createElement("a");
     btn.className = "delete";
     btn.innerHTML = "&times";
 
     btn.addEventListener("click", removeInput);
 
-    const flex=document.createElement("div");
-    flex.className="flex";
+    const flex = document.createElement("div");
+    flex.className = "flex";
     input.appendChild(flex);
 
-    var invi = document.createElement("input");;
+    var invi = document.createElement("input");
 
-    if(!!document.getElementsByName("ning")){
-
-        invi.type="hidden";
-        invi.name="ning";
-        invi.value=i;
-    }else{
-        invi2=document.getElementsByName("ning")
-        document.removeChild("ning");
-        invi.type="hidden";
-        invi.name="ning";
-        invi.value=invi2.value;
-    }
-
-    
     flex.appendChild(name);
     flex.appendChild(labelDesc);
     flex.appendChild(descripcion);
     flex.appendChild(btn);
     flex.appendChild(invi);
 
+}
+
+
+function esNumerico(value) {
+    return typeof value === 'number';
 }
 
 //addBtn.addEventListener("click", addInput);
